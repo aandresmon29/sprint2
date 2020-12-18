5ad7464d9ca293e5e6bd47ffa462c159820a8ff7
aandresmon29 <71345919+aandresmon29@users.noreply.github.com>
2020-12-17 22:06:40 -0500

    Update to sprint3

--- main.py
+++ main.py
@@ -7,23 +7,19 @@ from models.deadline_models import DlIn, DlOut
 import datetime
 from fastapi import FastAPI
 from fastapi import HTTPException
+from routers.entregas_router import router as router_entregas
 
 api = FastAPI()
 
-@api.get("/entregas/{nombre}")
-async def get_lentregas(nombre: str):
-    task_in_db = get_entrega(nombre)
-    dl_in_db = get_tarea(nombre)
-    if task_in_db == None:
-        raise HTTPException(status_code=404, detail="El objetivo no existe")
-    task_out = (EntOut(**task_in_db.dict()) , TaskOut(**dl_in_db.dict()))
-    return task_out
+from fastapi.middleware.cors import CORSMiddleware
 
-@api.put("/entregas/responsable/")
-async def get_lentregas(Ent_ent: EntIn):
-    task_in_db = get_entrega(Ent_ent.objetivo)
-    if task_in_db == None:
-        raise HTTPException(status_code=404, detail="El objetivo no existe")
-    task_in_db = update_entrega(Ent_ent)
-    task_out = get_entrega(Ent_ent.objetivo)
-    return task_out
+origins = [
+    "http://localhost.tiangolo.com", "https://localhost.tiangolo.com",
+    "http://localhost", "http://localhost:8080",
+]
+api.add_middleware(
+    CORSMiddleware, allow_origins=origins,
+    allow_credentials=True, allow_methods=["*"], allow_headers=["*"],
+)
+
+api.include_router(router_entregas)
---
+++ routers/entregas_router.py
@@ -0,0 +1,32 @@
+from typing import List
+from fastapi import Depends, APIRouter, HTTPException
+from sqlalchemy.orm import Session
+from db.deadline_db import get_deadline
+from db.entregas_db import get_entrega, update_entrega
+from db.tareas_db import get_tarea
+from models.tareas_models import TaskIn, TaskOut
+from models.entregas_models import EntIn, EntOut
+from models.deadline_models import DlIn, DlOut
+import datetime
+from fastapi import FastAPI
+from fastapi import HTTPException
+
+router = APIRouter()
+
+@router.get("/entregas/{nombre}")
+async def get_lentregas(nombre: str):
+    task_in_db = get_entrega(nombre)
+    dl_in_db = get_tarea(nombre)
+    if task_in_db == None:
+        raise HTTPException(status_code=404, detail="El objetivo no existe")
+    task_out = (EntOut(**task_in_db.dict()) , TaskOut(**dl_in_db.dict()))
+    return task_out
+
+@router.put("/entregas/responsable/")
+async def get_lentregas(Ent_ent: EntIn):
+    task_in_db = get_entrega(Ent_ent.objetivo)
+    if task_in_db == None:
+        raise HTTPException(status_code=404, detail="El objetivo no existe")
+    task_in_db = update_entrega(Ent_ent)
+    task_out = get_entrega(Ent_ent.objetivo)
+    return task_out
